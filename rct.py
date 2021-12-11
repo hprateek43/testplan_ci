9fc297b1bdeba1a09fdba0fc9641abbd087299dd
hprateek43 <harsh.prateek.singh@gmail.com>
2021-12-11 16:43:28 +0530

    Dynamic results

--- rct.py
+++ rct.py
@@ -1,11 +1,18 @@
 import sys
 import time
 import requests
+import random
 
 print(sys.argv[1])
 
 time.sleep(30)
 
+with open('results.csv',"w+") as f:
+    f.write("Avg,Median,90,min,max,samples,errors,error %")
+    line = str(random.uniform(500.00,530.00))+","+str(random.uniform(150.00,230.00))+","+str(random.uniform(900.00,1300.00))+","+str(random.uniform(0.500,3.00))+","+"16550,97560,360,0.37"
+    f.write()
+
+515.33,196,1117,2,16550,97560,360,0.37
 requests.post(
     url=sys.argv[1],
     data={
